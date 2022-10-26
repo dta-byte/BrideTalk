98d1de038778fddadf555318aa7821fbb5971165
JosefineKlyver <josefine.klyver@hotmail.com>
2022-10-26 17:27:40 +0200

    Login v1

--- bridetalk_project/src/components/pages/index.js
+++ bridetalk_project/src/components/pages/index.js
@@ -2,6 +2,9 @@ import { HomePage } from "./homepage/HomePage";
 import { LoginPage } from "./login/LogInPage";
 import { CreateUserPage } from "./create-user/CreateUserPage";
 
+
+
 export { CreateUserPage }
 export { HomePage }
 export { LoginPage }
+ 
