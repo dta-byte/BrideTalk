31f85c01e823e52ade0f445e4c24c6c6d3fb7bcd
emmathiesennielsen <emmathiesennielsen@gmail.com>
2022-11-06 19:15:48 +0100

    merge attempt

--- bridetalk_project/src/components/atoms/buttons/TestBack.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./cancelbutton.css";
-
-function TestBack(props){ 
-  return <button class="cancelbutton" onClick={()=>props.handleClick()}>{props.text}</button>;
-};
-
-export default TestBack;
-
