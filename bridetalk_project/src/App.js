adbd6ca4096c67b578da50e132754472ba5019d9
JosefineKlyver <josefine.klyver@hotmail.com>
2022-11-16 13:02:10 +0100

    typographies (virker ikke endnu)

--- bridetalk_project/src/App.js
+++ bridetalk_project/src/App.js
@@ -1,5 +1,6 @@
 import { MasterLayout } from "./components/pages/MasterLayout";
 import { initializeParse } from "./services/apiUtils"
+import "./css-system/typographies.css"
 import "./css-system/colors.css"
 
 function App() {
