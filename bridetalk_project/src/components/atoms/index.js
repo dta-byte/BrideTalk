72040d1b68431a64490099e82a32a3a68dd53756
Dahlia Andreasen <dahliata@icloud.com>
2022-11-17 17:22:59 +0100

    root css

--- bridetalk_project/src/components/atoms/index.js
+++ bridetalk_project/src/components/atoms/index.js
@@ -1,17 +1,8 @@
-import { Headline1 } from "./typographies/Headline1";
-import { Headline2 } from "./typographies/Headline2";
-import { Headline3 } from "./typographies/Headline3";
-import { ButtonText } from "./typographies/ButtonText";
-import { BodyText } from "./typographies/BodyText";
 import { InputField } from "./fillinbox/InputField";
 import { DropdownLocation } from "./fillinbox/DropdownLocation";
 import { Button } from "./buttons/Button"
 
-export { Headline1 }
-export { Headline2 }
-export { Headline3 }
-export { ButtonText }
-export { BodyText }
+
 export { Button }
 export { InputField }
 export { DropdownLocation }
