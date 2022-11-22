7f9739a57ab6414a5f1a220e7e5baf2e2ac5fc15
emmathiesennielsen <emmathiesennielsen@gmail.com>
2022-11-22 20:07:06 +0100

    update chatpage colors

--- bridetalk_project/src/components/molecule/ chatmolecule/livechatcomponent/LiveChatComponent.jsx
+++ bridetalk_project/src/components/molecule/ chatmolecule/livechatcomponent/LiveChatComponent.jsx
@@ -1,9 +1,23 @@
 import "./livechatcomponent.css";
-
+import { InputField } from "../../../atoms";
 export const LiveChatComponent = () => {
   return (
-    <div className="messagename">
-      <p type="text">Name</p>
+    <div className="flexbox-container-livechat">
+      <div className="flexchild1-livechat">
+        <div className="livechat-headline">Username</div>
+        <div
+          style={{
+            height: "1px",
+            width: "90%",
+            marginLeft: "5%",
+            backgroundColor: "black",
+            justifyContent: "center",
+          }}
+        />
+      </div>
+      <div className="flexchild2-livechat">
+        <InputField value="Aa" type="email" />
+      </div>
     </div>
   );
 };
--- bridetalk_project/src/components/pages/auth/components/AuthWrapper.jsx
+++ bridetalk_project/src/components/pages/auth/components/AuthWrapper.jsx
@@ -11,4 +11,3 @@ export const AuthWrapper = ({ children }) => {
   }
   return <Link to={"/login"} />;
 };
-
