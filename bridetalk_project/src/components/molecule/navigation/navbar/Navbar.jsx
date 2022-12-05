6a300ea4bc9c2c32eff519f899fdf59cdf48d91e
JosefineKlyver <josefine.klyver@hotmail.com>
2022-12-05 17:37:27 +0100

    nav-profile-icon dropdown

--- bridetalk_project/src/components/molecule/navigation/navbar/Navbar.jsx
+++ bridetalk_project/src/components/molecule/navigation/navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
 import { BsPersonCircle } from "react-icons/bs";
 import { BiMenu } from "react-icons/bi";
-
+import { useState } from "react";
 
 import "./navbar.css";
+import { DropdownProfile } from "../../../atoms";
 
 /*Functional component that creates the navigation bar.*/
 export const Navbar = () => {
-  
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <>
       <div className="topnav">
@@ -25,21 +31,43 @@ export const Navbar = () => {
 
         {/* Right side of navigation bar */}
         <div className="topnav-right">
-            {/* Profile icon */}
-          <Link to="/login">
-            <div className="profile-icon-column">
-              <BsPersonCircle className="profile-icon" size={30}/>
-            </div>
-
-          </Link>
-            {/* Menu icon */}
-            <Link to="/main-page">
-            <div className="menu-icon-column">
-              <BiMenu className="menu-icon" size={34}/>
+          <div className="nav-container">
+            <button
+              type="button"
+              class="nav-profile-button"
+              onClick={handleClick}
+            >
+              <div className="profile-icon-column2">
+                <BsPersonCircle className="profile-icon" size={35} />
               </div>
-            </Link>
+            </button>
+            <div style={{ visibility: isVisible ? "visible" : "hidden" }}>
+              <div class="nav-dropdown">
+                <ul>
+                  <li>Edit profile</li>
+                  <li>Help</li>
+                  <li>Log out</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
+
+/*    
+Profile icon
+<Link to="/login">
+<div className="profile-icon-column">
+  <BsPersonCircle className="profile-icon" size={30} />
+</div>
+</Link>
+Menu icon 
+<Link to="/main-page">
+<div className="menu-icon-column">
+  <BiMenu className="menu-icon" size={34} />
+</div>
+</Link>
+*/
